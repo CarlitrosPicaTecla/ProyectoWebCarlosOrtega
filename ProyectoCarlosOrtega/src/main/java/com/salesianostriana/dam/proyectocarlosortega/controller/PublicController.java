1791026cab63f87834a5dd73727568db57c686df
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2022-05-12 19:36:06 +0200

    casa

--- ProyectoCarlosOrtega/src/main/java/com/salesianostriana/dam/proyectocarlosortega/controller/PublicController.java
+++ ProyectoCarlosOrtega/src/main/java/com/salesianostriana/dam/proyectocarlosortega/controller/PublicController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PublicController {
 
-	@GetMapping("/inicio")
+	@GetMapping("/")
 	public String welcome() {
 		return "inicio";
 	}
